309ec0e347f93fe43381d4a7f0c976c750946aae
MyAlterEagle <73846118+MyAlterEagle@users.noreply.github.com>
2022-04-16 21:41:07 -0400

    Add files via upload

---
+++ FallingBlock.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tetris
+{
+    public static class FallingBlock
+    {
+        public static Color[] defColors = new Color[]
+        {
+            Color.Cyan,
+            Color.Blue,
+            Color.Orange,
+            Color.Yellow,
+            Color.Lime,
+            Color.Purple,
+            Color.Red
+        };
+
+        public static Vector2 block1 = new Vector2(20, 20);
+        public static Vector2 block2 = new Vector2(20, 20);
+        public static Vector2 block3 = new Vector2(20, 20);
+        public static Vector2 block4 = new Vector2(20, 20);
+
+        public static Color blockColor = Color.NavajoWhite;
+
+        private static Vector2 _origin;
+        public static Random rnd = new Random();
+
+        public static int nextBlock = 0;
+        public static int currentBlock;
+        public static int heldBlock;
+        public static bool canHold = true;
+
+        public static void NewBlock()
+        {
+            int type = nextBlock;
+            while (nextBlock == type)
+            {
+                nextBlock = rnd.Next(1, 7);
+            }
+
+            currentBlock = type;
+            canHold = true;
+        
+            switch (type)
+            {
+                case 1: //line | light blue
+                    blockColor = Color.Cyan;
+                    block1 = new Vector2(3, 1);
+                    block2 = new Vector2(4, 1);
+                    block3 = new Vector2(5, 1);
+                    block4 = new Vector2(6, 1);
+                    _origin = new Vector2(4.5f, 1.5f);
+                    break;
+                case 2: //reverse-L | blue
+                    blockColor = Color.Blue;
+                    block1 = new Vector2(3, 0);
+                    block2 = new Vector2(3, 1);
+                    block3 = new Vector2(4, 1);
+                    block4 = new Vector2(5, 1);
+                    _origin = new Vector2(4, 1);
+                    break;
+                case 3: //L | orange
+                    blockColor = Color.Orange;
+                    block1 = new Vector2(3, 1);
+                    block2 = new Vector2(4, 1);
+                    block3 = new Vector2(5, 1);
+                    block4 = new Vector2(5, 0);
+                    _origin = new Vector2(4, 1);
+                    break;
+                case 4://square | yellow
+                    blockColor = Color.Yellow;
+                    block1 = new Vector2(4, 0);
+                    block2 = new Vector2(4, 1);
+                    block3 = new Vector2(5, 0);
+                    block4 = new Vector2(5, 1);
+                    _origin = new Vector2(4.5f, 0.5f);
+                    break;
+                case 5://snake upwards | green
+                    blockColor = Color.Lime;
+                    block1 = new Vector2(3, 1);
+                    block2 = new Vector2(4, 1);
+                    block3 = new Vector2(4, 0);
+                    block4 = new Vector2(5, 0);
+                    _origin = new Vector2(4, 1);
+                    break;
+                case 6://T | purple
+                    blockColor = Color.Magenta;
+                    block1 = new Vector2(3, 1);
+                    block2 = new Vector2(4, 0);
+                    block3 = new Vector2(4, 1);
+                    block4 = new Vector2(5, 1);
+                    _origin = new Vector2(4, 1);
+                    break;
+                case 7://snake downwards | red
+                    blockColor = Color.Red;
+                    block1 = new Vector2(3, 0);
+                    block2 = new Vector2(4, 0);
+                    block3 = new Vector2(4, 1);
+                    block4 = new Vector2(5, 1);
+                    _origin = new Vector2(4, 1);
+                    break;
+                default://this shouldn't happen
+                    System.Diagnostics.Debug.WriteLine("ERROR: INVALID NEWBLOCK NUMBER");
+                    break;
+            }
+        }
+
+        public static void PushDown()
+        {
+            block1.Y += 1;
+            block2.Y += 1;
+            block3.Y += 1;
+            block4.Y += 1;
+            _origin.Y += 1;
+        }
+
+        public static void PushRight()
+        {
+            block1.X += 1;
+            block2.X += 1;
+            block3.X += 1;
+            block4.X += 1;
+            _origin.X += 1;
+        }
+
+        public static void PushLeft()
+        {
+            block1.X -= 1;
+            block2.X -= 1;
+            block3.X -= 1;
+            block4.X -= 1;
+            _origin.X -= 1;
+        }
+
+        public static void Drop(int y)
+        {
+            int lowY = (int)Math.Max(block1.Y, Math.Max(block2.Y, Math.Max(block3.Y, block4.Y)));
+
+            int dropBy = y - lowY;
+
+            block1.Y += dropBy;
+            block2.Y += dropBy;
+            block3.Y += dropBy;
+            block4.Y += dropBy;
+            _origin.Y += dropBy;
+
+        }
+
+        public static void Jump()
+        {
+            block1.Y -= 1;
+            block2.Y -= 1;
+            block3.Y -= 1;
+            block4.Y -= 1;
+        }
+
+        public static void Rotate(int degree)
+        {
+            float radians = (float)(Math.PI / 180 * degree);
+
+            block1 = Vector2.Transform(block1 - _origin, Matrix.CreateRotationZ(radians)) + _origin;
+            block2 = Vector2.Transform(block2 - _origin, Matrix.CreateRotationZ(radians)) + _origin;
+            block3 = Vector2.Transform(block3 - _origin, Matrix.CreateRotationZ(radians)) + _origin;
+            block4 = Vector2.Transform(block4 - _origin, Matrix.CreateRotationZ(radians)) + _origin;
+            FixLR();
+
+        }
+
+        public static void FixLR()
+        {
+            while ((int)Math.Round(block1.X) > 9
+                || (int)Math.Round(block2.X) > 9
+                || (int)Math.Round(block3.X) > 9
+                || (int)Math.Round(block4.X) > 9)
+            {
+                PushLeft();
+            }
+
+            while ((int)Math.Round(block1.X) < 0
+                || (int)Math.Round(block2.X) < 0
+                || (int)Math.Round(block3.X) < 0
+                || (int)Math.Round(block4.X) < 0)
+            {
+                PushRight();
+            }
+        }
+        public static void FixY()
+        {
+            while ((int)Math.Round(block1.Y) > 17
+                || (int)Math.Round(block2.Y) > 17
+                || (int)Math.Round(block3.Y) > 17
+                || (int)Math.Round(block4.Y) > 17)
+            {
+                Jump();
+            }
+        }
+
+        public static void HoldBlock()
+        {
+            if (canHold)
+            {
+                if (heldBlock == 0)
+                {
+                    heldBlock = currentBlock;
+                    NewBlock();
+                }
+                else
+                {
+                    int nextBlockSave = nextBlock;
+                    nextBlock = heldBlock;
+                    heldBlock = currentBlock;
+                    NewBlock();
+                    nextBlock = nextBlockSave;
+                }
+                canHold = false;
+            }
+        }
+
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,358 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Tetris
+{
+    public class Game1 : Game
+    {
+        
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D _backgroundTexture;
+        private Vector2 _backgroundPosition;
+
+        private Texture2D _cover;
+        private Texture2D[] _smallBlocks = new Texture2D[7];
+
+        private Texture2D _blockTexture;
+
+        private bool[,] _blockArray = new bool[10, 18];
+        private Color[,] _colorArray = new Color[10, 18];
+
+        private float _timer;
+        private int _lines = 0;
+        private int _score = 0;
+
+        private Color _heldColor;
+        
+        private bool _running;
+        private bool _blocksToBreak;
+        private bool _firstTry = true;
+
+        private KeyboardState _currentKey;
+        private KeyboardState _previousKey;
+
+        private SpriteFont _font;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            _graphics.PreferredBackBufferWidth = 843;
+            _graphics.PreferredBackBufferHeight = 900;
+            _graphics.ApplyChanges();
+        }
+
+        protected override void Initialize()
+        {
+            Window.Title = "Tetris";
+            _backgroundPosition = new Vector2(0, 0);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _backgroundTexture = Content.Load<Texture2D>("843x900background");
+            _blockTexture = Content.Load<Texture2D>("50x50block");
+            _cover = Content.Load<Texture2D>("83x16cover");
+            for (int i = 0; i < 7; i++)
+            {
+                _smallBlocks[i] = Content.Load<Texture2D>("50x50block" +(i+1));
+            }                                                                                       
+            _font = Content.Load<SpriteFont>("font");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            _previousKey = _currentKey;
+            _currentKey = Keyboard.GetState();
+            if (_currentKey.IsKeyDown(Keys.R) && _previousKey.IsKeyUp(Keys.R))
+            {
+                Restart();
+            }
+            if (!_running)
+            {
+                return;
+            }
+
+            _timer +=                                                                                                                                                                                                                                                                                                                                                                                                                                                                         (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (_blocksToBreak)
+            {
+                if (_timer > 0.1)
+                {
+                    for (int y = 0; y < 18; y++)
+                    {
+                        bool push = false;
+                        for (int x = 0; x < 10; x++)
+                        {
+                            if (_colorArray[x, y] == Color.DarkGray)
+                            {
+                                _blockArray[x, y] = false;
+                                _colorArray[x, y] = Color.Black;
+                                push = true;
+                            }
+                        }
+                        if (push)
+                        {
+                            _score += _lines * 20;
+                            _lines++;
+                            for (int yP = y; yP >= 0; yP--)
+                            {
+                                for (int xP = 0; xP < 10; xP++)
+                                {
+                                    try
+                                    {
+                                        _blockArray[xP, yP] = _blockArray[xP, yP - 1];
+                                        _colorArray[xP, yP] = _colorArray[xP, yP - 1];
+                                    }
+                                    catch
+                                    {
+
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                    _blocksToBreak = false;
+                    FallingBlock.NewBlock();
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            try
+            {
+                blockUnstucker();
+                blockPlacer();
+            }
+            catch
+            {
+                _firstTry = false;
+                _running = false;
+            }
+            try
+            {
+                runControls();
+            }
+            catch
+            {
+            }
+
+            float difficulty = 2.5f - _lines / 5;
+            if (difficulty < 0.25f)
+            {
+                difficulty = 0.5f;
+            }
+
+            if (_timer > difficulty)
+            {
+                FallingBlock.PushDown();
+                _timer = 0;
+            }
+            
+
+            base.Update(gameTime);
+        }
+
+        private void blockPlacer()
+        {
+            if ((int)Math.Max(FallingBlock.block1.Y, Math.Max(FallingBlock.block2.Y, Math.Max(FallingBlock.block3.Y, FallingBlock.block4.Y))) == 17
+                            || _blockArray[(int)Math.Round(FallingBlock.block1.X), (int)Math.Round(FallingBlock.block1.Y) + 1]
+                            || _blockArray[(int)Math.Round(FallingBlock.block2.X), (int)Math.Round(FallingBlock.block2.Y) + 1]
+                            || _blockArray[(int)Math.Round(FallingBlock.block3.X), (int)Math.Round(FallingBlock.block3.Y) + 1]
+                            || _blockArray[(int)Math.Round(FallingBlock.block4.X), (int)Math.Round(FallingBlock.block4.Y) + 1])
+            {
+                Color darkColor = new Color(FallingBlock.blockColor.R - 60, FallingBlock.blockColor.G - 60, FallingBlock.blockColor.B - 60);
+
+                _blockArray[(int)Math.Round(FallingBlock.block1.X), (int)Math.Round(FallingBlock.block1.Y)] = true;
+                _blockArray[(int)Math.Round(FallingBlock.block2.X), (int)Math.Round(FallingBlock.block2.Y)] = true;
+                _blockArray[(int)Math.Round(FallingBlock.block3.X), (int)Math.Round(FallingBlock.block3.Y)] = true;
+                _blockArray[(int)Math.Round(FallingBlock.block4.X), (int)Math.Round(FallingBlock.block4.Y)] = true;
+                _colorArray[(int)Math.Round(FallingBlock.block1.X), (int)Math.Round(FallingBlock.block1.Y)] = darkColor;
+                _colorArray[(int)Math.Round(FallingBlock.block2.X), (int)Math.Round(FallingBlock.block2.Y)] = darkColor;
+                _colorArray[(int)Math.Round(FallingBlock.block3.X), (int)Math.Round(FallingBlock.block3.Y)] = darkColor;
+                _colorArray[(int)Math.Round(FallingBlock.block4.X), (int)Math.Round(FallingBlock.block4.Y)] = darkColor;
+
+                bool anyLine = false;
+                for (int y = 0; y < 18; y++)
+                {
+                    bool line = true;
+                    for (int x = 0; x < 10; x++)
+                    {
+                        if (!_blockArray[x, y])
+                        {
+                            line = false;
+                            break;
+                        }
+                    }
+                    if (line)
+                    {
+                        anyLine = true;
+                        for (int x = 0; x < 10; x++)
+                        {
+                            _colorArray[x, y] = Color.DarkGray;
+                        }
+                        _timer = 0;
+                        _blocksToBreak = true;
+                    }
+                }
+                if (!anyLine)
+                {
+                    FallingBlock.NewBlock();
+                }
+                
+            }
+
+            
+        }
+
+        private void blockUnstucker()
+        {
+            while (_blockArray[(int)Math.Round(FallingBlock.block1.X), (int)Math.Round(FallingBlock.block1.Y)]
+                || _blockArray[(int)Math.Round(FallingBlock.block2.X), (int)Math.Round(FallingBlock.block2.Y)]
+                || _blockArray[(int)Math.Round(FallingBlock.block3.X), (int)Math.Round(FallingBlock.block3.Y)]
+                || _blockArray[(int)Math.Round(FallingBlock.block4.X), (int)Math.Round(FallingBlock.block4.Y)])
+            {
+                FallingBlock.Jump();
+            }
+        }
+
+        private void Restart()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 18; y++)
+                {
+                    _blockArray[x, y] = false;
+                }
+            }
+            _lines = 0;
+            _score = 0;
+            _running = true;
+            FallingBlock.heldBlock = 0;
+            FallingBlock.nextBlock = FallingBlock.rnd.Next(1, 7);
+            FallingBlock.NewBlock();
+        }
+
+        private void runControls()
+        {
+            if (_currentKey.IsKeyDown(Keys.Right) && _previousKey.IsKeyUp(Keys.Right)
+            && !_blockArray[(int)Math.Round(FallingBlock.block1.X) + 1, (int)Math.Round(FallingBlock.block1.Y)]
+            && !_blockArray[(int)Math.Round(FallingBlock.block2.X) + 1, (int)Math.Round(FallingBlock.block2.Y)]
+            && !_blockArray[(int)Math.Round(FallingBlock.block3.X) + 1, (int)Math.Round(FallingBlock.block3.Y)]
+            && !_blockArray[(int)Math.Round(FallingBlock.block4.X) + 1, (int)Math.Round(FallingBlock.block4.Y)])
+            {
+                FallingBlock.PushRight();
+                FallingBlock.FixLR();
+            }
+            else if (_currentKey.IsKeyDown(Keys.Left) && _previousKey.IsKeyUp(Keys.Left)
+            && !_blockArray[(int)Math.Round(FallingBlock.block1.X) - 1, (int)Math.Round(FallingBlock.block1.Y)]
+            && !_blockArray[(int)Math.Round(FallingBlock.block2.X) - 1, (int)Math.Round(FallingBlock.block2.Y)]
+            && !_blockArray[(int)Math.Round(FallingBlock.block3.X) - 1, (int)Math.Round(FallingBlock.block3.Y)]
+            && !_blockArray[(int)Math.Round(FallingBlock.block4.X) - 1, (int)Math.Round(FallingBlock.block4.Y)])
+            {
+                FallingBlock.PushLeft();
+                FallingBlock.FixLR();
+            }
+
+            if (_currentKey.IsKeyDown(Keys.Up) && _previousKey.IsKeyUp(Keys.Up))
+            {
+                FallingBlock.Rotate(90);
+                FallingBlock.FixY();
+            }
+            if (_currentKey.IsKeyDown(Keys.Z) && _previousKey.IsKeyUp(Keys.Z))
+            {
+                FallingBlock.Rotate(-90);
+                FallingBlock.FixY();
+            }
+            if (_currentKey.IsKeyDown(Keys.Down) && _previousKey.IsKeyUp(Keys.Down))
+            {
+                FallingBlock.PushDown();
+                _score += 10;
+            }
+            if (_currentKey.IsKeyDown(Keys.Space) && _previousKey.IsKeyUp(Keys.Space))
+            {
+                int lowPoint = (int)Math.Round(Math.Max(FallingBlock.block1.Y, Math.Max(FallingBlock.block2.Y, Math.Max(FallingBlock.block3.Y, FallingBlock.block4.Y))));
+                for (int i = lowPoint; i < 18; i++)
+                {
+                    if (i == 17)
+                    {
+                        _score += 20*(17-lowPoint);
+                        FallingBlock.Drop(17);
+                    }
+                    else if (_blockArray[(int)Math.Round(FallingBlock.block1.X), i + 1 - (lowPoint-(int)FallingBlock.block1.Y)]
+                        || _blockArray[(int)Math.Round(FallingBlock.block2.X), i + 1 - (lowPoint - (int)FallingBlock.block2.Y)]
+                        || _blockArray[(int)Math.Round(FallingBlock.block3.X), i + 1 - (lowPoint - (int)FallingBlock.block3.Y)]
+                        || _blockArray[(int)Math.Round(FallingBlock.block4.X), i + 1 - (lowPoint - (int)FallingBlock.block4.Y)])
+                    {
+                        FallingBlock.Drop(i);
+                        _score += 20 * (i - lowPoint);
+                        break;
+                    }
+
+                }
+                FallingBlock.FixY();
+            }
+            if (_currentKey.IsKeyDown(Keys.C) && _previousKey.IsKeyUp(Keys.C))
+            {
+                _heldColor = FallingBlock.blockColor;
+                FallingBlock.HoldBlock();
+            }
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_backgroundTexture, _backgroundPosition, Color.White);
+
+            if (_running || _firstTry)
+            {
+                _spriteBatch.Draw(_cover, new Vector2(521, 356), Color.White);
+            }
+
+            if (FallingBlock.heldBlock != 0)
+            {
+                _spriteBatch.Draw(_smallBlocks[FallingBlock.heldBlock-1], new Vector2(716, 66), FallingBlock.defColors[FallingBlock.heldBlock-1]);
+            }
+            if (FallingBlock.nextBlock != 0)
+            {
+                _spriteBatch.Draw(_smallBlocks[FallingBlock.nextBlock - 1], new Vector2(548, 66), FallingBlock.defColors[FallingBlock.nextBlock - 1]);
+            }
+
+            _spriteBatch.DrawString(_font, _score.ToString(), new Vector2(573, 119), Color.Black);
+            _spriteBatch.DrawString(_font, _lines.ToString(), new Vector2(573, 139), Color.Black);
+
+            _spriteBatch.Draw(_blockTexture, Vector2.Multiply(FallingBlock.block1, 50), FallingBlock.blockColor);
+            _spriteBatch.Draw(_blockTexture, Vector2.Multiply(FallingBlock.block2, 50), FallingBlock.blockColor);
+            _spriteBatch.Draw(_blockTexture, Vector2.Multiply(FallingBlock.block3, 50), FallingBlock.blockColor);
+            _spriteBatch.Draw(_blockTexture, Vector2.Multiply(FallingBlock.block4, 50), FallingBlock.blockColor);
+
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 18; y++)
+                {
+                    if (_blockArray[x, y])
+                    {
+                        _spriteBatch.Draw(_blockTexture, new Vector2(x * 50, y * 50), _colorArray[x, y]);
+                    }
+                }
+            }
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
